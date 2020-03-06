5d8a9377e6f44175723c4774d6f6fafa4f5a430c
Upendra Pandey <b117060@iiit-bh.ac.in>
2020-03-06 15:38:41 -0800

    Updates

---
+++ precache-manifest.79c7cca3eb687aab5f16c42c7200e8f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab37db7fb1bbd1405bde7b1bf6c6b2af",
+    "url": "/playfree/index.html"
+  },
+  {
+    "revision": "926beefde1af9ffdbc8d",
+    "url": "/playfree/static/css/main.404cdb1b.chunk.css"
+  },
+  {
+    "revision": "769d0dd1b8be10211919",
+    "url": "/playfree/static/js/2.8a8986d0.chunk.js"
+  },
+  {
+    "revision": "ea25659f102c6425053f0c1e0a54af5a",
+    "url": "/playfree/static/js/2.8a8986d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "926beefde1af9ffdbc8d",
+    "url": "/playfree/static/js/main.6c4f3018.chunk.js"
+  },
+  {
+    "revision": "aecc715c348bd5cc1f6b",
+    "url": "/playfree/static/js/runtime-main.89397ed1.js"
+  }
+]);
